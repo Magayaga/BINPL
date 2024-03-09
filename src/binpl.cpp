ba1e5b0209b512d9d2850a2ebb9a82d24ec238b7
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-03-09 19:10:10 +0800

    Update `binpl.cpp`

--- src/binpl.cpp
+++ src/binpl.cpp
@@ -220,43 +220,38 @@ void interpretBinary(const std::string &binaryCode, int decimalMode, int hexadec
 
 int main(int argc, char *argv[]) {
     if (argc != 2 && argc != 3) {
-        std::cout << "Usage: binpl [binary file]\n";
+        std::cout << "Usage: binpl [binary file]" << std::endl;
         return 1;
     }
 
     // Check for help option
     if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
-        std::cout << "Usage: binpl [binary file] [-d or --decimalMode] [-h or --hexadecimal]\n"
-                  << "       -v or --version       Version information.\n"
-                  << "       -h or --help          Usage information.\n"
-                  << "       -d or --decimal       Interpret binary as decimal numbers.\n"
-                  << "       -h or --hexadecimal   Interpret binary as hexadecimal numbers.\n"
-                  << "       --author              Author information.";
+        std::cout << "Usage: binpl [binary file] [-d or --decimalMode] [-h or --hexadecimal]" << std::endl;
+        std::cout << "       -v or --version       Version information." << std::endl;
+        std::cout << "       -h or --help          Usage information." << std::endl;
+        std::cout << "       -d or --decimal       Interpret binary as decimal numbers." << std::endl;
+        std::cout << "       -h or --hexadecimal   Interpret binary as hexadecimal numbers." << std::endl;
+        std::cout << "       --author              Author information." << std::endl;
         return 0;
     }
-    
+
     else if (argc == 2 && (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0)) {
         std::cout << BINPL_VERSION << std::endl;
         return 0;
     }
-    
+
     else if (argc == 2 && (strcmp(argv[1], "--author") == 0)) {
-        std::cout << "Copyright (c) 2024 Cyril John Magayaga\n";
+        std::cout << "Copyright (c) 2024 Cyril John Magayaga" << std::endl;
         return 0;
     }
 
     std::ifstream file(argv[1], std::ios::binary);
     if (!file.is_open()) {
-        std::cerr << "Error: Unable to open file '" << argv[1] << "'\n";
+        std::cerr << "Error: Unable to open file '" << argv[1] << "'" << std::endl;
         return 1;
     }
 
-    file.seekg(0, std::ios::end);
-    std::streampos fileSize = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    std::string binaryCode(fileSize, '\0');
-    file.read(&binaryCode[0], fileSize);
+    std::string binaryCode((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     file.close();
 
     int decimalMode = 0; // Default mode: ASCII interpretation
@@ -266,13 +261,13 @@ int main(int argc, char *argv[]) {
         if (strcmp(argv[2], "-d") == 0 || strcmp(argv[2], "--decimal") == 0) {
             decimalMode = 1;
         }
-        
+
         else if (strcmp(argv[2], "-h") == 0 || strcmp(argv[2], "--hexadecimal") == 0) {
             hexadecimalMode = 1;
         }
-        
+
         else {
-            std::cerr << "Error: Invalid option '" << argv[2] << "'\n";
+            std::cerr << "Error: Invalid option '" << argv[2] << "'" << std::endl;
             return 1;
         }
     }
@@ -287,7 +282,7 @@ void interpretBinary(const std::string &binaryCode, int decimalMode, int hexadec
 
     // Make sure the length is a multiple of 8
     if (len % 8 != 0) {
-        std::cerr << "Error: Invalid binary string length\n";
+        std::cerr << "Error: Invalid binary string length" << std::endl;
         exit(1);
     }
 
@@ -300,11 +295,11 @@ void interpretBinary(const std::string &binaryCode, int decimalMode, int hexadec
         if (decimalMode) {
             std::cout << decimalValue << " ";
         }
-        
+
         else if (hexadecimalMode) {
-            std::cout << std::hex << std::uppercase << decimalValue << " ";
+            std::cout << std::hex << decimalValue << " ";
         }
-        
+
         else {
             std::cout << static_cast<char>(decimalValue);
         }
